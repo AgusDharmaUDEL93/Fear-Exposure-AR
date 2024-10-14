1910e27fb2cf0de4841e6a34cb039b4c07aecd00
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-10-14 11:58:18 +0700

    Initial Commit

---
+++ Fear Exposure AR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fear Exposure AR
+//
+//  Created by Putu Agus Dharma Kusuma on 14/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fear Exposure AR/Fear_Exposure_ARApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fear_Exposure_ARApp.swift
+//  Fear Exposure AR
+//
+//  Created by Putu Agus Dharma Kusuma on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Fear_Exposure_ARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
