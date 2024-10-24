4bc65452c017a213980e32270b14b7c814f37eb7
Steven <songkowidjojo@student.ciputra.ac.id>
2024-10-24 09:45:54 +0700

    fix: update PicturePlaygroundScreen
    - Delete reset button
    - Modify zoom gesture to work like slider

--- Fear Exposure AR/Presentation/Pages/PicturePlayground/PicturePlaygroundScreen.swift
+++ Fear Exposure AR/Presentation/Pages/PicturePlayground/PicturePlaygroundScreen.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct PicturePlaygroundScreen : View {
     
     @State var viewModel = PicturePlaygroundViewModel()
+    @State private var magnification: CGFloat = 1.0
+    let minZoom: CGFloat = 0
+    let maxZoom: CGFloat = 1
     
     var body: some View {
         GeometryReader { geometry in
@@ -18,40 +21,58 @@ struct PicturePlaygroundScreen : View {
                     .resizable()
                     .scaledToFit()
                     .font(.title)
-                    .padding(60)
+                    .padding(80)
                     .scaleEffect(viewModel.currentZoom + viewModel.totalZoom)
+                
                 VStack  {
                     Spacer()
-                    Slider(value: $viewModel.currentZoom)
+                    
+                    HStack {
+                        Text("-")
+                            .font(.title)
+                        
+                        Slider(value: $viewModel.currentZoom)
+                            .accentColor(Color(Theme.primary500.rawValue))
+                        
+                        Text("+")
+                            .font(.title)
+                    }
+                    .padding(.horizontal, 4)
+                    
                     Spacer()
                         .frame(height: 32)
-                    Button(action: {
-                        viewModel.onResetZoom()
-                    }, label: {
-                        Text("Reset")
-                            .font(.body)
-                            .bold()
-                            .frame(maxWidth: geometry.size.width)
-                            .padding(.vertical, 6)
-                    })
-                    .padding(.bottom, 48)
-                    .buttonStyle(.borderedProminent)
                 }
                 .padding(.horizontal, 16)
             }
             .frame(maxWidth: geometry.size.width, maxHeight: geometry.size.height)
             .background()
-//            .gesture(
-//                MagnificationGesture()
-//                    .onChanged { value in
-//                        viewModel.onChangedZoom(value)
-//                    }
-//                    .onEnded { value in
-//                        viewModel.onEndedZoom()
-//                    }
-//            )
+            .gesture(
+                MagnificationGesture()
+                    .onChanged { value in
+                        let delta = value - magnification
+                        
+                        // Apply zoom with bounds check
+                        let newZoom = viewModel.currentZoom + delta
+                        if newZoom >= minZoom && newZoom <= maxZoom {
+                            viewModel.currentZoom = newZoom
+                        }
+                        
+                        magnification = value
+                    }
+                    .onEnded { _ in
+                        magnification = 1.0 // reset magnification to default after gesture ends
+                    }
+            )
+            //            .gesture(
+            //                MagnificationGesture()
+            //                    .onChanged { value in
+            //                        viewModel.onChangedZoom(value)
+            //                    }
+            //                    .onEnded { value in
+            //                        viewModel.onEndedZoom()
+            //                    }
+            //            )
         }
-        
         .ignoresSafeArea(.all)
     }
 }
